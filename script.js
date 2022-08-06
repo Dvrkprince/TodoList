d1b3ce4154933443c4add0ee606bdc40658af1f5
Arif Mohamed <arifmohamec32@gmail.com>
2022-08-05 21:45:49 -0400

    Added div Element, Completed button, and p Element

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const addButton = document.getElementsByClassName("addToListButton")[0];
 const listToAdd = document.getElementsByClassName("list-to-add")[0];
-let addedList = document.getElementsByClassName("test")[0];
+let i = 0;
 
 //Addbutton
 addButton.addEventListener("click", function(){
-    console.log(listToAdd.innerText);
-    addedList.innerText = listToAdd.value;
+    console.log(listToAdd.innerText);//testing purposes
+
+    //created div element store the new p and button
+    let listContainer = document.createElement('div');
+    listContainer.className = "listContainer";
+    document.getElementsByClassName("lists-added")[0].appendChild(listContainer);
+
+    //create an html element to store the list text
+    let list = document.createElement('p');
+    list.className = "newList";
+    //take the text entered by the user and store it into a list
+    list.textContent = listToAdd.value;
+    //attach the list to the html div of lists
+    document.getElementsByClassName("listContainer")[i].appendChild(list);
+
+    //created Completed button
+    let btn = document.createElement("button");
+    btn.className = "completedButton";
+    btn.innerHTML = "Completed";
+    document.getElementsByClassName("listContainer")[i].appendChild(btn); 
+
+    i++;   
   });
+
+
